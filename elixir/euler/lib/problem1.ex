cf6ce031612bd43278a66b3ad133f000fb6c0c54
Wade Mauger <wade_mauger@intuit.com>
2016-09-24 13:06:20 -0700

    Remove duplication

--- elixir/euler/lib/problem1.ex
+++
@@ -1,9 +0,0 @@
-defmodule Problem1 do
-
-  def solve do
-    Enum.reduce((1..999), 0, fn(num, acc) -> if( rem(num, 3)==0 || rem(num, 5)==0, do: acc + num, else: acc) end)
-  end
-
-end
-
-IO.puts Problem1.solve()
--- elixir/euler/test/euler_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule EulerTest do
-  use ExUnit.Case
-  doctest Problem1
-
-  test "the truth" do
-    assert 1 + 1 == 2
-    assert 2 + 2 == 4
-  end
-end
