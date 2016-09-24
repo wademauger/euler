adbb67bd550992a58656f5a09d52048c809c44e3
Wade Mauger <wade_mauger@intuit.com>
2016-09-23 21:08:38 -0700

    Problem 3

--- elixir/problem1.ex
+++ elixir/problem1.ex
@@ -1,2 +1,9 @@
-multiples = Enum.reduce((1..999), 0, fn(number, accumulator) -> if( rem(number, 3)==0 || rem(number, 5)==0, do: accumulator + number, else: accumulator) end)
-IO.puts multiples
+defmodule Problem1 do
+
+  def solve do
+    Enum.reduce((1..999), 0, fn(num, acc) -> if( rem(num, 3)==0 || rem(num, 5)==0, do: acc + num, else: acc) end)
+  end
+
+end
+
+IO.puts Problem1.solve()
